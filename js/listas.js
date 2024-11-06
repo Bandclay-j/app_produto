9e1d7bbafdabded3770c707ef1a5b3cc9aa9c1b1
Talisson-Rodrigues <talisontalison57@gmail.com>
2024-11-06 11:24:38 -0300

    Atualização

--- js/listas.js
+++ js/listas.js
@@ -1,25 +1,94 @@
+let produtoIdParaExcluir = null; //Variável para armazenar o ID do produto a ser excluído
+let produtoIdParaEditar = null; //Variável para armazenar o ID do produto a ser editado
+
 function getAllProdutos() {
     axios.get('http://localhost:8080/produto')
     .then(function(response) {
         console.log(response);
-
         var jsonData = response.data;
         var tableBody = $("table tbody");
-
         jsonData.forEach(produto => {
-            var markup = "<tr>" + 
+            var markup = "<tr style='text-align: center'>" + 
                          "<td>" + produto.id + "</td>" +
                          "<td>" + produto.nome + "</td>" +
                          "<td>" + produto.preco + "</td>" +
                          "<td>" + produto.dataCadastro + "</td>" +
                          "<td>" + (produto.categoria ? produto.categoria.nome : "")+ "</td>" +
                          "<td>" + (produto.fornecedor ? produto.fornecedor.nome : "")+ "</td>" +
+                         "<td>" + "<button class='btn btn-danger' onclick='confirmarExclusao(" + produto.id + ")'>Excluir</button>" + 
+                         "<button class='btn btn-primary' onclick='editarProduto(" + produto.id + ")' style='margin-left: 10px'>Editar</button>" + "</td>" +
                          "</tr>";
-
             tableBody.append(markup);
         });
     })
+    .catch(function(error) {
+        console.log(error);
+    });
 }
+
+function confirmarExclusao(id) {
+    // Armazena o ID do produto e exibe o modal de confirmação
+    produtoIdParaExcluir = id;
+    $('#modalExcluirProduto').modal('show');
+}
+
+document.getElementById("confirmarExclusao").addEventListener("click", function() {
+    if (produtoIdParaExcluir) {
+        // Chama a função de exclusão com o ID armazenado
+        axios.delete('http://localhost:8080/produto/' + produtoIdParaExcluir)
+        .then(function(response) {
+            alert("Produto deletado com sucesso!");
+            $('#modalExcluirProduto').modal('hide'); //Fecha o modal após a exclusão
+            location.reload(); // Recarrega a página para atualizar a lista de produtos
+        })
+        .catch(function(error) {
+            console.log(error);
+            alert("Erro ao deletar o produto!");
+        });
+    }
+});
+
+function editarProduto(id) {
+    produtoIdParaEditar = id;
+
+    axios.get('http://localhost:8080/produto/' + id)
+    .then(function(response) {
+        const produto = response.data;
+
+        document.getElementById("editarNome").value = produto.nome;
+        document.getElementById("editarPreco").value = produto.preco;
+        document.getElementById("editarDataCadastro").value = produto.dataCadastro;
+
+        $('#modalEditarProduto').modal('show');
+    })
+    .catch(function(error) {
+        console.log(error);
+        alert("Erro ao buscar o produto!");
+    });
+}
+
+document.getElementById("salvarAlteracoes").addEventListener("click", function() {
+    const nome = document.getElementById("editarNome").value;
+    const preco = document.getElementById("editarPreco").value;
+    const dataCadastro = document.getElementById("editarDataCadastro").value;
+
+    const produtoAtualizado = {
+        nome: nome,
+        preco: preco,
+        dataCadastro: new Date().toISOString().split("T")[0]
+    };
+
+    axios.put('http://localhost:8080/produto/' + produtoIdParaEditar, produtoAtualizado)
+    .then(function(response) {
+        alert("Produto atualizado com sucesso!");
+        $('#modalEditarProduto').modal('hide');
+        location.reload();
+    })
+    .catch(function(error) {
+        console.log(error);
+        alert("Erro ao atualizar o produto!");
+    });
+});
 
 function getAllCategorias() {
     axios.get('http://localhost:8080/categoria')
@@ -30,10 +99,10 @@ function getAllCategorias() {
         var tableBody = $("table tbody");
 
         jsonData.forEach(categoria => {
-            var markup = "<tr>" + 
-                         "<td>" + categoria.id + "</td>" +
-                         "<td>" + categoria.nome + "</td>" +
-                         "</tr>";
+            var markup = "<tr>" +
+                            "<td>" + categoria.id + "</td>" +
+                            "<td>" + categoria.nome + "</td>" +
+                            "</tr>";
 
             tableBody.append(markup);
         });
