6d7a1a048906f671d47340c5cf17653190f3bd64
Talisson-Rodrigues <talisontalison57@gmail.com>
2024-11-07 11:09:32 -0300

    Mais

--- js/listas.js
+++ js/listas.js
@@ -108,3 +108,25 @@ function getAllCategorias() {
         });
     })
 }
+
+function getAllLojas() {
+    axios.get('http://localhost:8080/loja')
+    .then(function(response) {
+        console.log(response);
+
+        var jsonData = response.data;
+        var tableBody = $("table tbody");
+
+        jsonData.forEach(loja => {
+            var markup = "<tr>" +
+                            "<td>" + loja.id + "</td>" +
+                            "<td>" + loja.nome + "</td>" +
+                            "<td>" + loja.endereco + "</td>" +
+                            "<td>" + loja.latitude + "</td>" +
+                            "<td>" + loja.longitude + "</td>" +
+                            "</tr>";
+
+            tableBody.append(markup);
+        });
+    })
+}
--- js/mapas.js
+++ js/mapas.js
@@ -1,14 +1,34 @@
-// Inicialize o mapa e defina a posição central e o nível de zoom
-const map = L.map('map').setView([-15.812743484997265, -47.89300692050525], 16);
+// Inicializa o mapa e define a posição central e o nível de zoom
+const map = L.map('map').setView([-15.812743484997265, -47.89300692050525], 12);
 
-// Adicione a camada do OpenStreetMap ao mapa
+// Adiciona a camada do OpenStreetMap ao mapa
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   maxZoom: 19,
   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-// Adicione um marcador em Brasília
-const marker = L.marker([-15.812743484997265, -47.89300692050525])
-  .addTo(map)
-  .bindPopup("<b>Loja 1 de Brasília</b>")
-  .openPopup();
+// Função para carregar as lojas do backend e adicionar os marcadores ao mapa
+async function carregarLojas() {
+  try {
+    // Faz uma requisição ao endpoint /loja para obter as lojas
+    const response = await fetch('http://localhost:8080/loja');  // Substitua pela URL do seu backend
+    const lojas = await response.json();
+
+    // Itera pelas lojas e cria um marcador para cada uma
+    lojas.forEach(loja => {
+      const { latitude, longitude, nome } = loja;  // Extrai os dados de cada loja
+
+      // Cria um marcador para cada loja
+      L.marker([latitude, longitude])
+        .addTo(map)
+        .bindPopup(`<b>${nome}</b>`)
+        .openPopup();
+    });
+  } catch (error) {
+    console.error("Erro ao carregar lojas:", error);
+  }
+}
+
+// Carrega as lojas assim que o mapa for inicializado
+carregarLojas();
+
